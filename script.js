98d8fedba2f45a633509bec09beab068d2b8c9e8
E.S. Luper <eric.s.luper@protonmail.com>
2022-05-09 16:43:41 -0500

    Feature: Implemented W3 Schools Image Slider
    -Implemented W3 Schools Image Slider
    -Removed Obsolete JS

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 var previousTab = 'home-page';
-document.addEventListener("click", deleteLogo);
+
+let slideIndex = 1;
+showSlides(slideIndex);
 
 function swapMiddle(p,n){
     document.getElementById(p).style.display='none';
@@ -16,3 +18,29 @@ function hideSideBar(){
     document.getElementById('side-nav-bar').style.display='none';
     document.getElementById('filter').style.display='none';
 }
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+} 
